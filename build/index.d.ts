a529440e92f7127c839aa7765ffdcc67ad49cafa
Brian Takita <brian.takita@gmail.com>
2024-01-01 19:02:35 -0500

    # relysjs
    ## 0.33.4
    
    ### Patch Changes
    
    - build: fix: exports from rebuildjs/build:
    
    build_id$_
    build_id_
    build_id__set
    build_id__refresh
    persist__metafile__build_id$_
    persist__metafile__build_id_
    persist__metafile__ready$_
    persist__metafile__ready_
    rebuildjs__build_id$_
    rebuildjs__build_id_
    rebuildjs__build_id__set
    rebuildjs__ready$_
    rebuildjs__ready_
    rebuildjs__ready__wait
    server__external_
    rebuildjs_plugin_
    
    ## 0.33.3
    
    ### Patch Changes

--- build/index.d.ts
+++ build/index.d.ts
@@ -1,8 +1,25 @@
 import type { ctx__be_T, ctx__get_T, ctx__set_T, sig_T } from 'ctx-core/rmemo'
 import type { BuildContext, Plugin } from 'esbuild'
 import type { has_app_T } from 'rebuildjs'
-import type { rebuildjs_build_config_T, rebuildjs_plugin_ } from 'rebuildjs/build'
-export { rebuildjs_plugin_ }
+import type { rebuildjs_build_config_T } from 'rebuildjs/build'
+export {
+	build_id$_,
+	build_id_,
+	build_id__set,
+	build_id__refresh,
+	persist__metafile__build_id$_,
+	persist__metafile__build_id_,
+	persist__metafile__ready$_,
+	persist__metafile__ready_,
+	rebuildjs__build_id$_,
+	rebuildjs__build_id_,
+	rebuildjs__build_id__set,
+	rebuildjs__ready$_,
+	rebuildjs__ready_,
+	rebuildjs__ready__wait,
+	server__external_,
+	rebuildjs_plugin_,
+} from 'rebuildjs/build'
 export declare const relysjs__build_id$_:ctx__be_T<has_app_T, sig_T<string>, 'app'>
 export declare const relysjs__build_id_:ctx__get_T<has_app_T, string>
 export declare const relysjs__build_id__set:ctx__set_T<has_app_T, string>
