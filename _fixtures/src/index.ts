a8cf6c5a0797302b782ca92ba225c65436dd9147
Brian Takita <brian.takita@gmail.com>
2024-01-28 01:37:43 -0500

    _fixtures: ∋ is_entry_file_: synchronous usage

--- _fixtures/src/index.ts
+++ _fixtures/src/index.ts
@@ -1,9 +1,6 @@
 import { is_entry_file_ } from 'ctx-core/fs'
 import { relysjs_browser__build, relysjs_server__build } from '../../server/build/index.js'
-is_entry_file_(import.meta.url, process.argv[1])
-	.then(async is_entry_file=>{
-		if (is_entry_file) {
-			await relysjs_server__build()
-			await relysjs_browser__build()
-		}
-	})
+if (is_entry_file_(import.meta.url, process.argv[1])) {
+	await relysjs_server__build()
+	await relysjs_browser__build()
+}
